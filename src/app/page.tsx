4a07ceda5f96ba71dc2540dd4b64e5872d33d11f
Iury V <iurygol@gmail.com>
2023-07-07 21:38:50 -0300

    feat: add recommended trips component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
-import QuickSearch from "@/components/QuickSearch";
+import QuickSearch from "@/app/components/QuickSearch";
 import TripSearch from "./components/TripSearch";
+import RecommendedTrips from "./components/RecommendedTrips";
 
 export default function Home() {
   return (
     <div>
       <TripSearch />
       <QuickSearch />
+      <RecommendedTrips />
     </div>
   );
 }
