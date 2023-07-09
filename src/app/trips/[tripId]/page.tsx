a6226ecd10b7a9d0883937933e02e24183d00b8d
Iury V <iurygol@gmail.com>
2023-07-08 22:26:52 -0300

    feat: add trip description component

--- src/app/trips/[tripId]/page.tsx
+++ src/app/trips/[tripId]/page.tsx
@@ -3,6 +3,7 @@ import { prisma } from "@/lib/prisma";
 import React from "react";
 import TripHeader from "./components/TripHeader";
 import TripReservation from "./components/TripReservation";
+import TripDescription from "./components/TripDescription";
 
 
 const getTripDetails = async (tripId: string) => {
@@ -24,9 +25,8 @@ const TripDetails = async ({ params }: { params: { tripId: string } }) => {
     <div className="container mx-auto">
  
       <TripHeader trip={trip}/>
-
-      {/* RESERVA */}
       <TripReservation trip={trip}/>
+      <TripDescription description={trip.description}/>
     </div>
   );
 };
